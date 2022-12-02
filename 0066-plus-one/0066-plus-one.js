efebecb8a0c6118787191ca578e8010f38ca0804
NyiNyi LayHtet <nnlayhtet@gmail.com>
2022-12-02 16:45:34 -0500

    Time: 107 ms (21.58%), Space: 42 MB (60.15%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+
+    let pointer = digits.length - 1
+    digits[pointer] += 1
+    
+    while (pointer !== 0){
+        if (digits[pointer] >= 10) {
+            digits[pointer] = 0
+            digits[pointer - 1] += 1
+        }
+    pointer--
+    }
+    if (digits[0] === 10){
+        digits[0] = 0
+        digits.unshift(1)
+    }
+    return digits   
+};
