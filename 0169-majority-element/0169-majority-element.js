a009c52c5e5b8cf41e9bf63f0ad3bfbc3caa002a
NyiNyi LayHtet <nnlayhtet@gmail.com>
2022-12-02 12:27:29 -0500

    Time: 109 ms (57.61%), Space: 45.7 MB (17.90%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let counter = {}
-    for (let i=0; i<nums.length; i++){
-        counter[nums[i]] = (counter[nums[i]] || 0) + 1
-        if (counter[nums[i]] > nums.length/2) return nums[i]
-    }
-    
+    // let counter = {}
+    // for (let i=0; i<nums.length; i++){
+    //     counter[nums[i]] = (counter[nums[i]] || 0) + 1
+    //     if (counter[nums[i]] > nums.length/2) return nums[i]
+    // }
+    let sortedArray = nums.sort()
+    return sortedArray[Math.floor(sortedArray.length/2)]    
     
 };
