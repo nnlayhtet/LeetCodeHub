5e024df9c3dabec4871ad7ca351d5478f0c2987e
NyiNyi LayHtet <nnlayhtet@gmail.com>
2023-09-29 14:02:33 -0400

    Time: 76 ms (66.80%), Space: 53.2 MB (52.65%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -6,16 +6,16 @@ var isMonotonic = function(nums) {
   let ascendingCount = 0
   let descendingCount = 0
   if(nums.length === 1) return true
-    for(let i = 0, j = 1; i < nums.length - 1; i++, j++){
-      if(nums[i] <= nums[j]){
+    for(let i = 0; i < nums.length - 1; i++){
+      if(nums[i] <= nums[i + 1]){
         ascendingCount++
       }
       if(ascendingCount === nums.length - 1){
         return true
       }
     }
-    for(let i = 0, j = 1; i < nums.length - 1; i++, j++){
-      if(nums[i] >= nums[j]){
+    for(let i = 0; i < nums.length - 1; i++){
+      if(nums[i] >= nums[i + 1]){
         descendingCount++
       }
       if(descendingCount === nums.length - 1){
