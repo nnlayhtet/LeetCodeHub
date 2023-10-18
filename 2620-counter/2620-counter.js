ea0a5de23c834a40777d2867a2b6a5f766a34ce8
NyiNyi LayHtet <nnlayhtet@gmail.com>
2023-10-18 15:39:00 -0400

    Time: 48 ms (68.84%), Space: 41.7 MB (64.42%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+  let countUp = -1
+    
+  return function() {
+    countUp++
+    return n + countUp
+  };
+
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
