513c2424916a26745f0d43db7b19ffb201d8b02c
NyiNyi LayHtet <nnlayhtet@gmail.com>
2023-10-23 16:27:23 -0400

    Time: 70 ms (11.24%), Space: 43.3 MB (63.05%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+  if (n === 1) return true;
+  if (n < 1) return false;
+  if (n <= 4) return n % 4 === 0;
+  return isPowerOfFour(n / 4);
+};
