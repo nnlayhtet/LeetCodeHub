233e771262796d8c5f7bdbaec537a9daa62d0603
NyiNyi LayHtet <nnlayhtet@gmail.com>
2023-10-24 17:13:03 -0400

    Time: 6448 ms (5.01%), Space: 52.4 MB (5.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,7 +5,14 @@
 var maxProfit = function (prices) {
   let max = 0;
   let difference;
-  let maxPossibleDifference = Math.max(...prices) - Math.min(...prices);
+  let maxInArray = Math.max(...prices);
+  let minInArray = Math.min(...prices);
+  let maxPossibleDifference = maxInArray - minInArray;
+
+  if (prices.indexOf(minInArray) < prices.indexOf(maxInArray)){
+    return maxPossibleDifference;
+  }
+
   for (let i = 0; i < prices.length - 1; i++) {
     for (let j = i + 1; j < prices.length; j++) {
       difference = prices[j] - prices[i];
