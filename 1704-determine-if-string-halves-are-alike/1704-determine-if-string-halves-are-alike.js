0d02b9764d8c9ad4898f22839be3919c8bea3256
NyiNyi LayHtet <nnlayhtet@gmail.com>
2024-01-12 13:36:30 -0500

    Time: 67 ms (22.11%), Space: 43.9 MB (41.75%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -6,13 +6,13 @@ var halvesAreAlike = function(s) {
   const vowels = new Set(['a','e','i','o','u'])
   let aCount = 0
   let bCount = 0
-  let a = s.slice(0,s.length/2)
-  let b = s.slice(s.length/2)
+  let a = s.slice(0,s.length/2).toLowerCase()
+  let b = s.slice(s.length/2).toLowerCase()
   for(let char of a){
-    if (vowels.has(char.toLowerCase())) aCount++
+    if (vowels.has(char)) aCount++
   }
   for(let char of b){
-    if (vowels.has(char.toLowerCase())) bCount++
+    if (vowels.has(char)) bCount++
   }
   return aCount === bCount
 };
