d0a2419b922e1f2b58016d4332e59ced9aa1cfe3
NyiNyi LayHtet <nnlayhtet@gmail.com>
2024-02-22 15:29:07 -0500

    Time: 86 ms (67.85%), Space: 57.6 MB (40.00%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -6,21 +6,20 @@
 var findJudge = function (n, trust) {
   let canBeJudge = new Set([]);
   let cannotBeJudge = new Set([]);
+  let count = 0;
 
-  for (let i=0; i<n; i++){
-    canBeJudge.add(i+1)
+  for (let i = 0; i < n; i++) {
+    canBeJudge.add(i + 1);
   }
   for (let pair of trust) {
     cannotBeJudge.add(pair[0]);
   }
-  for (const element of cannotBeJudge) {
-    canBeJudge.delete(element);
+  for (const person of cannotBeJudge) {
+    canBeJudge.delete(person);
   }
-    let count =0
-    let j = [...canBeJudge][0]
-    for (let e of trust){
-        if(e[1] === j) count++
-    }
-    
-  return n-1===count? j : -1
+  let j = [...canBeJudge][0];
+  for (let e of trust) {
+    if (e[1] === j) count++;
+  }
+  return n - 1 === count ? j : -1;
 };
