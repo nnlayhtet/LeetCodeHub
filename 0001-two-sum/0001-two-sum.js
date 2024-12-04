20f993bad1d4d49a97fda0136fc38ff6e5119fc0
NyiNyi LayHtet <nnlayhtet@gmail.com>
2024-12-04 16:08:03 -0500

    Time: 38 ms (45.23%), Space: 49.6 MB (80.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,23 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let foundIdx;
-    let difference;
-    for(let i = 0; i < nums.length; i++){
-        difference = target - nums[i]
-        foundIdx = nums.indexOf(difference, i+1)
-        if(foundIdx !== -1) return [i, foundIdx] 
+    for (let i =0; i<nums.length; i++) {
+        let current = nums[i]
+        let pairIndex = nums.indexOf(target-current,i+1)
+        if (pairIndex !== -1) return [i,pairIndex]
     }
+    return []
 };
-// var twoSum = function(nums, target) {
-// 	let map = {};
-	
-// 	for(let i = 0; i < nums.length; i++) {
-// 		const n = nums[i];
-// 		if(map[target - n] !== undefined) {
-// 			return [map[target - n], i];
-// 		}
-// 		map[n] = i;
-// 	}
-// 	return [];
-// }
